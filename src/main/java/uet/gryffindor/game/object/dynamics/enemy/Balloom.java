f02c830a77a2941421f95c8712927ae410e90b6c
johnhoangsoftware <johnhoangsoftware@gmail.com>
2021-11-22 00:32:43 +0700

    [update] balloom sửa tên, thêm hàm move

--- src/main/java/uet/gryffindor/game/object/dynamics/enemy/Balloom.java
+++ src/main/java/uet/gryffindor/game/object/dynamics/enemy/Balloom.java
@@ -1,7 +1,8 @@
-package uet.gryffindor.game.object.dynamic.enemy;
+package uet.gryffindor.game.object.dynamics.enemy;
 
 import javafx.beans.property.DoubleProperty;
 import javafx.beans.property.SimpleDoubleProperty;
+import uet.gryffindor.autopilot.GameAction;
 import uet.gryffindor.game.Manager;
 import uet.gryffindor.game.base.OrderedLayer;
 import uet.gryffindor.game.base.Vector2D;
@@ -10,17 +11,14 @@ import uet.gryffindor.game.engine.Collider;
 import uet.gryffindor.game.engine.Input;
 import uet.gryffindor.game.object.DynamicObject;
 
-public class Ballom extends DynamicObject {
-  private DoubleProperty speed;
-
+public class Balloom extends DynamicObject {
+  private DoubleProperty speed; //tốc độ cho từng enemy
   private boolean isBlocked = false;
   private Vector2D oldPosition;
 
   @Override
   public void start() {
-    Manager.INSTANCE.getGame().getCamera().setFocusOn(this);
-    speed = new SimpleDoubleProperty(6f);
-
+    speed = new SimpleDoubleProperty(8f);
     orderedLayer = OrderedLayer.MIDGROUND;
     oldPosition = position.clone();
   }
@@ -34,13 +32,35 @@ public class Ballom extends DynamicObject {
   }
 
   private void move() {
-    int street = (int) Math.random() * 10;
+    int value = ((int) (Math.random() * 100)) % 4 ;
+    // random hướng cho enemy.
+    switch (GameAction.valueOf(value)) {
+      case UP:
+        this.position.y -= speed.get();
+        // load texture
+        break;
+      case DOWN:
+        this.position.y += speed.get();
+        // load texture
+        break;
+      case RIGHT:
+        this.position.x += speed.get();
+        // load texture
+        break;
+      case LEFT:
+        this.position.x -= speed.get();
+        // load texture: texture.changeTo("name");
+        break;
+      default:
+        texture.pause();
+        break;
+    }
 
   }
 
   @Override
   public void onCollisionEnter(Collider that) {
-    if (that.gameObject instanceof Unmovable) { // nếu bomber va chạm với vật thể tĩnh
+    if (that.gameObject instanceof Unmovable) { // nếu balloom va chạm với vật thể tĩnh
       position = oldPosition.smooth(this.dimension.x); // khôi phục vị trí trước khi va chạm
       isBlocked = true; // gắn nhãn bị chặn
     }
